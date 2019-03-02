ad14174f58eadb11cd65b216fe540e4ee160e328
mr-fool <mr-fool@users.noreply.github.com>
2019-03-02 14:42:45 -0700

    added some debugging console.log

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {manager: ' ' };
+    this.state = {manager: '' };
   }
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
     this.setState({manager});
+    console.log(manager);
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
   }
-
+  
   render() {
     return (
       <div>
