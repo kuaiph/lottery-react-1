269b6d7e932f0ad4520fdd6e39cae0fbe2fe5804
mr-fool <mr-fool@users.noreply.github.com>
2019-03-04 00:16:30 -0700

    added event handler

--- src/App.js
+++ src/App.js
@@ -29,6 +29,19 @@ class App extends Component {
     this.setState({ manager, web3, lottery,players, balance });
   }
  
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const lottery = new web3.eth.Contract(
+      contractData.abi,
+      contractData.address
+    );
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+  }
+
   render() {
     return (
       <div>
@@ -39,7 +52,7 @@ class App extends Component {
           competiting to win { web3.utils.fromWei(this.state.balance, 'ether') } ether! 
           </p>
       <hr/>
-      <form>
+      <form onSubmit={this.onSubmit}>
         <h4>Want to try your luck?</h4>
         <div>
           <label>Amount of ether to enter</label>
