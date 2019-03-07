ee94a864e19a5281b9521f4a0a2f8197641cb8de
mr-fool <mr-fool@users.noreply.github.com>
2019-03-06 17:54:00 -0700

    adding build

---
+++ build/precache-manifest.82308ff8757de53a98315d2e263bb335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cd7ded8d99c7207e6c3",
+    "url": "/static/css/main.a85484ba.chunk.css"
+  },
+  {
+    "revision": "7cd7ded8d99c7207e6c3",
+    "url": "/static/js/main.7cd7ded8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b2cb0a460803286c74d3",
+    "url": "/static/js/2.b2cb0a46.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "29c0556a17fe8563df403f85e3e60afe",
+    "url": "/index.html"
+  }
+];
